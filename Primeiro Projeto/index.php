513b0234a5640bdc81ff12b4d5791583b41aca10
RaelGomes <99209385+RaelGomes@users.noreply.github.com>
2023-02-27 16:44:55 -0300

    Add files via upload

---
+++ Primeiro Projeto/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Ola Mundo";
+        ?>
+    </h1>
+
+    <h2>
+    1+1 =
+        <?php
+            echo (1+1)
+        ?>
+    </h2>
+
+    <h2>
+    2+2 =
+        <?php
+            echo (2+2)
+        ?>
+    </h2>
+
+    <h2>
+    3+3 =
+        <?php
+            echo (3+3)
+        ?>
+    </h2>
+
+    <h2>
+    4+4 =
+        <?php
+            echo (4+4)
+        ?>
+    </h2>
+
+</body>
+</html>
